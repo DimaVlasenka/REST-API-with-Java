61412cd0e2df44dd0452bd70cc1109c4470c1b1f
Dima Vlasenka <dmbelwood@gmail.com>
2023-01-09 21:06:32 -0500

    Basics class notes updates

--- src/Basics.java
+++ src/Basics.java
@@ -1,11 +1,8 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
-
-import static org.hamcrest.Matchers.*;
-
+import static org.hamcrest.Matchers.*; //package needed for equalTo() method
 import files.payload;
-
-import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.*; //package needed for given() method
 
 public class Basics {
 
@@ -13,7 +10,7 @@ public class Basics {
 		
 		//to validate if Add place API works as expected
 		
-		//Given - all input details
+		//Given - all input details query/path parameters, header, body
 		//When - submit the API - resource and http method are specified here 
 		//Then - validate the response
 		
@@ -21,11 +18,12 @@ public class Basics {
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 		
 		//given, when, then implementation
-		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-		.body(payload.AddPlace())
-		.when().post("/maps/api/place/add/json")
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json").body(payload.AddPlace())
+		.when().post("maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
 		.header("Server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
+		
+		// Add place -> Update place with new address -> Get place to validate if new address is presented in response
 		
 		System.out.println(response);
 		
