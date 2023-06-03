8b2cbc0d76335c6bc25f2fa826e5a476b77d969d
Dima Vlasenka <dmbelwood@gmail.com>
2023-06-03 16:38:03 -0400

    Delete Order

--- src/ECommerceAPITest.java
+++ src/ECommerceAPITest.java
@@ -13,74 +13,112 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.testng.Assert;
+
 public class ECommerceAPITest {
 
 	public static void main(String[] args) {
-	
-	//Login	
-	RequestSpecification reqBasic = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").setContentType(ContentType.JSON).build();
-	
-	LoginRequest loginRequest = new LoginRequest();
-	loginRequest.setUserEmail("dima@gmail.com");
-	loginRequest.setUserPassword("Dm_123456");
-	
-	RequestSpecification reqLogin = given().log().all().spec(reqBasic).body(loginRequest);
-	
-	LoginResponse loginResponse = reqLogin.when().log().all().post("/api/ecom/auth/login").then().log().all().extract().response().as(LoginResponse.class);
-	System.out.println(loginResponse.getToken());
-	System.out.println(loginResponse.getUserId());
-	
-	String userID = loginResponse.getUserId();
-	String userToken = loginResponse.getToken();
-	
-	//Add product //multiPart() is used to add attachment
-	
-	RequestSpecification reqAddPoductBasic = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addHeader("Authorization", userToken).build();
-			
-	RequestSpecification reqAddProduct = given().log().all().spec(reqAddPoductBasic).
-			param("productName", "LaptopDimona").
-			param("productAddedBy", userID).
-			param("productCategory", "electronic").
-			param("productSubCategory", "Laptops").
-			param("productPrice","1795").
-			param("productDescription", "DimonaNewBrand").
-			param("productFor","Everybody").
-			multiPart("productImage", new File("C://Users//DV//eclipse-workspace//DemoProject//src//files//laptop.jpg"));
-	
-	
-String addPRoductResponse =	reqAddProduct.when().log().all().post("/api/ecom/product/add-product").then().log().all().extract().response().asString();
 
-JsonPath js = new JsonPath(addPRoductResponse);
+		// Login
+		RequestSpecification reqBasic = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.setContentType(ContentType.JSON).build();
 
-String productID = js.get("productId");
+		LoginRequest loginRequest = new LoginRequest();
+		loginRequest.setUserEmail("dima@gmail.com");
+		loginRequest.setUserPassword("Dm_123456");
 
-System.out.println(productID);
+		// NOTE relaxedHTTPSValidation() is used to bypass SSL certification
+		RequestSpecification reqLogin = given().relaxedHTTPSValidation().log().all().spec(reqBasic).body(loginRequest);
 
-	// URL to access UI part https://rahulshettyacademy.com/client "userEmail": "dima@gmail.com", "userPassword": "Dm_123456"
-	
-	//Create Order
+		LoginResponse loginResponse = reqLogin.when().log().all().post("/api/ecom/auth/login").then().log().all()
+				.extract().response().as(LoginResponse.class);
+		System.out.println(loginResponse.getToken());
+		System.out.println(loginResponse.getUserId());
 
-RequestSpecification reqCreateOrderBasic = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").
-addHeader("Authorization", userToken).setContentType(ContentType.JSON).build();
+		String userID = loginResponse.getUserId();
+		String userToken = loginResponse.getToken();
 
-OrderDetails orderDetail = new OrderDetails();
-orderDetail.setCountry("Germany");
-orderDetail.setProductOrderedId(productID);
+		// Add product //multiPart() is used to add attachment
 
-List<OrderDetails> listOrderDetails = new ArrayList<OrderDetails>();
-listOrderDetails.add(orderDetail);
+		RequestSpecification reqAddPoductBasic = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addHeader("Authorization", userToken).build();
 
-Orders orders = new Orders();
-orders.setOrders(listOrderDetails);
+		RequestSpecification reqAddProduct = given().log().all().spec(reqAddPoductBasic)
+				.param("productName", "LaptopDimona").param("productAddedBy", userID)
+				.param("productCategory", "electronic").param("productSubCategory", "Laptops")
+				.param("productPrice", "1795").param("productDescription", "DimonaNewBrand")
+				.param("productFor", "Everybody").multiPart("productImage",
+						new File("C://Users//DV//eclipse-workspace//DemoProject//src//files//laptop.jpg"));
 
-RequestSpecification reqCreateOrder = given().log().all().spec(reqCreateOrderBasic).body(orders);
+		String addPRoductResponse = reqAddProduct.when().log().all().post("/api/ecom/product/add-product").then().log()
+				.all().extract().response().asString();
 
-String createOrderResponse = reqCreateOrder.when().post("/api/ecom/order/create-order").
-then().log().all().extract().response().asString();
+		JsonPath js = new JsonPath(addPRoductResponse);
 
+		String productID = js.get("productId");
 
+		System.out.println(productID);
 
+		// URL to access UI part https://rahulshettyacademy.com/client "userEmail":
+		// "dima@gmail.com", "userPassword": "Dm_123456"
 
+		// Create Order
+
+		RequestSpecification reqCreateOrderBasic = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com")
+				.addHeader("Authorization", userToken).setContentType(ContentType.JSON).build();
+
+		OrderDetails orderDetail = new OrderDetails();
+		orderDetail.setCountry("Germany");
+		orderDetail.setProductOrderedId(productID);
+
+		List<OrderDetails> listOrderDetails = new ArrayList<OrderDetails>();
+		listOrderDetails.add(orderDetail);
+
+		Orders orders = new Orders();
+		orders.setOrders(listOrderDetails);
+
+		RequestSpecification reqCreateOrder = given().log().all().spec(reqCreateOrderBasic).body(orders);
+
+		String createOrderResponse = reqCreateOrder.when().post("/api/ecom/order/create-order").then().log().all()
+				.extract().response().asString();
+		
+		JsonPath js1 = new JsonPath(createOrderResponse);
+		List <String> ListOrderID = js1.getList("orders");
+		
+		String orderID = ListOrderID.get(0);
+		
+		System.out.println(orderID);
+		
+		//Delete Product
+		
+		RequestSpecification deleteProdBasic = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").
+				addHeader("Authorization", userToken).setContentType(ContentType.JSON).build();
+		
+		RequestSpecification deleteProdReq = given().log().all().spec(deleteProdBasic).pathParam("productID", productID);
+		
+		String deleteProdResponse = deleteProdReq.when().delete("/api/ecom/product/delete-product/{productID}").
+				then().log().all().extract().response().asString();
+		
+		JsonPath js2 = new JsonPath(deleteProdResponse);
+		String deleteMessage = js2.getString("message");
+		
+		Assert.assertEquals(deleteMessage, "Product Deleted Successfully");
+		
+		//Delete Order
+		
+		RequestSpecification deleteOrderBasic = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").
+				addHeader("Authorization", userToken).setContentType(ContentType.JSON).build();
+		
+		RequestSpecification deleteOrderReq = given().spec(deleteOrderBasic).pathParam("OrderID", orderID);
+		
+		String deleteOrderResponse = deleteOrderReq.when().delete("https://rahulshettyacademy.com/api/ecom/order/delete-order/{OrderID}").then().log().all().extract().response().asString();
+		
+		JsonPath js3 = new JsonPath(deleteOrderResponse);
+		
+		String deleteOrderMessage = js3.getString("message");
+		
+		Assert.assertEquals(deleteOrderMessage, "Orders Deleted Successfully");
+		
 	}
 
 }
